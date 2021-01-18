269793c9e798a2f5839855a93bd71773aefc74ab
richdartingfruit <76633464+richdartingfruit@users.noreply.github.com>
2021-01-17 20:02:51 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+    if (receivedNumber == 1) {
+        RingbitCar.back()
+    } else if (receivedNumber == 2) {
+        RingbitCar.forward()
+    } else if (receivedNumber == 3) {
+        RingbitCar.brake()
+    } else if (receivedNumber == 4) {
+        RingbitCar.forward()
+    } else if (receivedNumber == 5) {
+        RingbitCar.turnleft()
+    } else if (receivedNumber == 6) {
+        RingbitCar.turnright()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    RingbitCar.back()
+})
+input.onButtonPressed(Button.B, function () {
+    RingbitCar.brake()
+})
+music.playTone(262, music.beat(BeatFraction.Whole))
+basic.showIcon(IconNames.LeftTriangle)
+radio.setGroup(1)
+RingbitCar.init_wheel(AnalogPin.P2, AnalogPin.P1)
+basic.forever(function () {
+	
+})
